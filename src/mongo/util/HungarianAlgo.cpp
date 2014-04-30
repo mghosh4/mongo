6c4e2f8cad018ebddfd88ac3dee9631b22b9699a
wenting <wenting@ubuntu.(none)>
2014-04-30 14:31:38 -0500

    Update Hungarian algo with max-input

--- src/mongo/util/HungarianAlgo.cpp
+++ src/mongo/util/HungarianAlgo.cpp
@@ -6,21 +6,25 @@
 #include <string.h>
 void HungarianAlgo::max_cost_assignment(long long ** datainkr, int numRow, int numColumn,int assignment[])
 {
-
-    //#ifdef DEBUG
-    std::cout << "Munkres input matrix:" << std::endl;
-      for (  int row = 0 ; row < numRow ; row++ ) {
+    long long max=0;     
+    for (  int row = 0 ; row < numRow ; row++ ) {
         for (  int col = 0 ; col < numColumn ; col++ ) {
-              datainkr[row][col]=0-datainkr[row][col];
-          std::cout.width(8);
-          std::cout << datainkr[row][col] << ",";
+            if(max<datainkr[row][col])
+                max = datainkr[row][col];
+        }
+    }
+    
+    for (  int row = 0 ; row < numRow ; row++ ) {
+        for (  int col = 0 ; col < numColumn ; col++ ) {
+            datainkr[row][col]=max-datainkr[row][col];
+            std::cout.width(8);
+            std::cout << datainkr[row][col] << ",";
               
         }
         std::cout << std::endl;
-      }
-      std::cout << std::endl;
-    //#endif
-        
+    }
+    std::cout << std::endl;
+	
     min_cost_assignment(datainkr,numRow,numColumn,assignment);
 }
 void HungarianAlgo::min_cost_assignment(long long **datainkr, int numRow, int numColumn, int assignment[])
